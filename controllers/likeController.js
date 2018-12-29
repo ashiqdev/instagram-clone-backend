2aa9342209aae76f6b3552b2f5c1cda054eb824e
Ashik <ashiquejava@gmail.com>
2018-12-29 21:29:18 +0600

    like/unlike and bookmark/unbookmark are completed :)

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -32,31 +32,6 @@ exports.likePost = async (req, res) => {
 };
 
 
-// exports.unlike = async (req, res) => {
-//   try {
-//     const liked = await Like.findOne({
-//       post: req.body.postId,
-//       like_by: req.user.id,
-//     });
-
-//     if (liked) {
-//       await Like.findOneAndRemove({ like_by: req.user.id });
-//       res.json({
-//         sucess: true,
-//       });
-//     } else {
-//       res.json({
-//         notLiked: 'You have not yet liked this post',
-//       });
-//     }
-//   } catch (err) {
-//     res.json({
-//       postNotFound: 'post was not found',
-//     });
-//   }
-// };
-
-
 exports.unlike = async (req, res) => {
   try {
     const liked = await Like.findOne({
